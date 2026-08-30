31ef9353c52743dbb9eb769e4246539e27b233b5
Marwin Jade <jcardinez03>
2026-08-30 17:46:47 +0800

    updated cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://business-management-system-three.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
