56e5e77e08860398cf929d40d103fcbf4e5d2834
Marwin Jade <jcardinez03>
2026-09-05 16:59:57 +0800

    remove roles id from business_user table

--- database/migrations/2026_08_09_152943_create_business_user_table.php
+++ database/migrations/2026_08_09_152943_create_business_user_table.php
@@ -14,12 +14,11 @@ return new class extends Migration
         Schema::create('business_user', function (Blueprint $table) {
             $table->unsignedBigInteger('business_id');
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('role_id')
-                ->default(1);
+          
 
             $table->foreign('business_id')->references('id')->on('businesses');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('role_id')->references('id')->on('roles');
+          
         });
     }
 
