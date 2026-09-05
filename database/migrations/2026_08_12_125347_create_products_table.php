50caf5587cfea2158d96008008edad84c31a6a15
Marwin Jade <jcardinez03>
2026-09-05 17:48:27 +0800

    sku renamed to lowercase

--- database/migrations/2026_08_12_125347_create_products_table.php
+++ database/migrations/2026_08_12_125347_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('SKU');
+            $table->string('sku');
             $table->decimal('cost', 10,2);
             $table->decimal('selling_price', 10,2);
             $table->decimal('competitor_price', 10,2)->nullable();
